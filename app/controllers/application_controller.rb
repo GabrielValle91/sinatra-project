bd99fed92773b1f865ddd207e5c3a82690b4d9df
Gabriel Valle <gvalle91@gmail.com>
2018-07-12 20:29:36 +0000

    added index page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < Sinatra::Base
 
+  config do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "programmingisfun"
+  end
+
+  get '/' do
+    erb :index
+  end
 end
