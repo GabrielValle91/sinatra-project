e1b93bb7b48cad46d01b3be215cdaf7b7cd37982
Gabriel Valle <gvalle91@gmail.com>
2018-07-13 14:44:39 +0000

    setup shipment index page

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,3 +1,11 @@
 class TransactionController < ApplicationController
 
+  get '/transactions/shipping' do
+    if logged_in?
+      @current_user
+      erb :"transactions/shipping/index"
+    else
+      redirect '/login'
+    end
+  end
 end
