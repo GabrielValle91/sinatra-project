6d4bd7f2be6c836d675faf96d797cae49189f610
Gabriel Valle <gvalle91@gmail.com>
2018-07-13 18:04:03 +0000

    implemented client index page, started client view page

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -4,6 +4,7 @@ class ClientController < ApplicationController
 
   get '/clients' do
     if logged_in?
+
       erb :"clients/index"
     else
       redirect "/login"
@@ -18,18 +19,32 @@ class ClientController < ApplicationController
     end
   end
 
+  get '/clients/:id' do
+    if logged_in?
+      @client = Client.find(params[:id])
+      if @client.user_id = current_user.id
+        erb :"clients/show"
+      else
+        redirect '/clients'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
   post '/clients' do
-    client = Client.new
-    client.name = params[:client_name]
-    client.user_id = current_user.id
-    #binding.pry
     if !Client.where('name = ? AND user_id = ?', params[:client_name], current_user.id).empty?
       flash.now[:notice] = "There is already a client with that name"
       erb :"clients/new"
-    elsif client.save
-      redirect '/clients'
     else
-      redirect '/clients/new'
+      client = Client.new
+      client.name = params[:client_name]
+      client.user_id = current_user.id
+      if client.save
+        redirect '/clients'
+      else
+        redirect '/clients/new'
+      end
     end
   end
 end
