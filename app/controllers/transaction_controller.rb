1b6df6603f24382b57019a774dfe6dccdab68ec2
Gabriel Valle <gvalle91@gmail.com>
2018-07-14 18:47:54 +0000

    finalized shipping transaction show and edit pages

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -16,7 +16,34 @@ class TransactionController < ApplicationController
     end
   end
 
-  post '/transactions' do
+  get '/transactions/shipping/:id/edit' do
+    if logged_in?
+      @transaction = Transaction.find(params[:id])
+      if @transaction.client.user = current_user
+        #binding.pry
+        erb :"transactions/shipping/edit"
+      else
+        redirect '/transactions/shipping'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/transactions/shipping/:id' do
+    if logged_in?
+      @transaction = Transaction.find(params[:id])
+      if @transaction.client.user = current_user
+        erb :"transactions/shipping/show"
+      else
+        redirect '/transactions/shipping'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/transactions/shipping' do
     if !Transaction.where('reference = ? AND client_id = ?', params[:transaction_reference], params[:client_id])
       flash.now[:notice] = "That client already has a transaction with that reference"
       erb :"transactions/shipping/new"
@@ -33,5 +60,28 @@ class TransactionController < ApplicationController
         redirect '/transactions/shipping/new'
       end
     end
+  end
+
+  patch '/transactions/shipping/:id' do
+    @transaction = Transaction.find(params[:id])
+    params[:products].each do |product, quantity|
+      if quantity.first.to_i > 0
+        if lineitem = LineItem.find_by(:transaction_id => @transaction.id, :product_id => product.to_i)
+          lineitem.quantity = quantity.first.to_i
+          lineitem.save
+        else
+          lineitem = LineItem.new
+          lineitem.transaction_id = @transaction.id
+          lineitem.product_id = product.to_i
+          lineitem.quantity = quantity.first.to_i
+          lineitem.save
+        end
+      elsif quantity.first.to_i == 0 || !quantity.first
+        if lineitem = LineItem.find_by(:transaction_id => @transaction.id, :product_id => product.to_i)
+          lineitem.delete
+        end
+      end
+    end
+    redirect "/transactions/shipping/#{@transaction.id}"
   end
 end
