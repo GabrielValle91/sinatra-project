ed4d5672eb341918463a47eed4de6a2a6a9520f8
Gabriel Valle <gvalle91@gmail.com>
2018-07-14 18:56:48 +0000

    added receiving actions to transaction controller

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -1,5 +1,6 @@
 class TransactionController < ApplicationController
 
+    #controller action for shipping transactions
   get '/transactions/shipping' do
     if logged_in?
       erb :"transactions/shipping/index"
@@ -20,7 +21,6 @@ class TransactionController < ApplicationController
     if logged_in?
       @transaction = Transaction.find(params[:id])
       if @transaction.client.user = current_user
-        #binding.pry
         erb :"transactions/shipping/edit"
       else
         redirect '/transactions/shipping'
@@ -83,5 +83,90 @@ class TransactionController < ApplicationController
       end
     end
     redirect "/transactions/shipping/#{@transaction.id}"
+  end
+
+  #controller action for receiving transactions
+  get '/transactions/receiving' do
+    if logged_in?
+      erb :"transactions/receiving/index"
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/transactions/receiving/new' do
+    if logged_in?
+      erb :"transactions/receiving/new"
+    else
+      redirect "/login"
+    end
+  end
+
+  get '/transactions/receiving/:id/edit' do
+    if logged_in?
+      @transaction = Transaction.find(params[:id])
+      if @transaction.client.user = current_user
+        erb :"transactions/receiving/edit"
+      else
+        redirect '/transactions/receiving'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/transactions/receiving/:id' do
+    if logged_in?
+      @transaction = Transaction.find(params[:id])
+      if @transaction.client.user = current_user
+        erb :"transactions/receiving/show"
+      else
+        redirect '/transactions/receiving'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/transactions/receiving' do
+    if !Transaction.where('reference = ? AND client_id = ?', params[:transaction_reference], params[:client_id])
+      flash.now[:notice] = "That client already has a transaction with that reference"
+      erb :"transactions/receiving/new"
+    else
+      transaction = Transaction.new
+      transaction.reference = params[:transaction_reference]
+      transaction.client_id = params[:client_id]
+      transaction.transaction_type = "receiving"
+      transaction.transaction_date = params[:transaction_date]
+      transaction.status = "open"
+      if transaction.save
+        redirect  '/transactions/receiving'
+      else
+        redirect '/transactions/receiving/new'
+      end
+    end
+  end
+
+  patch '/transactions/receiving/:id' do
+    @transaction = Transaction.find(params[:id])
+    params[:products].each do |product, quantity|
+      if quantity.first.to_i > 0
+        if lineitem = LineItem.find_by(:transaction_id => @transaction.id, :product_id => product.to_i)
+          lineitem.quantity = quantity.first.to_i
+          lineitem.save
+        else
+          lineitem = LineItem.new
+          lineitem.transaction_id = @transaction.id
+          lineitem.product_id = product.to_i
+          lineitem.quantity = quantity.first.to_i
+          lineitem.save
+        end
+      elsif quantity.first.to_i == 0 || !quantity.first
+        if lineitem = LineItem.find_by(:transaction_id => @transaction.id, :product_id => product.to_i)
+          lineitem.delete
+        end
+      end
+    end
+    redirect "/transactions/receiving/#{@transaction.id}"
   end
 end
