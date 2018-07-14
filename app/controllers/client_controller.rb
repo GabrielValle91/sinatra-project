3bc532297124f03775d33a648045089f16f1db93
Gabriel Valle <gvalle91@gmail.com>
2018-07-14 15:26:11 +0000

    added client edit page

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -18,6 +18,19 @@ class ClientController < ApplicationController
     end
   end
 
+  get '/clients/:id/edit' do
+    if logged_in?
+      @client = Client.find(params[:id])
+      if @client.user_id == current_user.id
+        erb :"clients/edit"
+      else
+        redirect '/clients'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
   get '/clients/:id' do
     if logged_in?
       @client = Client.find(params[:id])
@@ -43,6 +56,22 @@ class ClientController < ApplicationController
         redirect '/clients'
       else
         redirect '/clients/new'
+      end
+    end
+  end
+
+  patch '/clients/:id' do
+    if !Client.where('name = ? AND user_id = ?', params[:client_name], current_user.id).empty?
+      flash.now[:notice] = "There is already a client with that name"
+      @client = Client.find(params[:id])
+      erb :"clients/edit"
+    else
+      client = Client.find(params[:id])
+      client.name = params[:client_name]
+      if client.save
+        redirect '/clients'
+      else
+        redirect "/clients/#{params[:id]}/edit"
       end
     end
   end
