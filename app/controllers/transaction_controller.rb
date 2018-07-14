0f464353d4f1a787beba007c1c1f1e3871a94a77
Gabriel Valle <gvalle91@gmail.com>
2018-07-14 16:15:42 +0000

    added new shipping transaction page, fixed some other select elements

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -3,6 +3,7 @@ class TransactionController < ApplicationController
   get '/transactions/shipping' do
     if logged_in?
       @current_user
+      @transactions = @current_user.transactions.where('type = shipping AND status = open')
       erb :"transactions/shipping/index"
     else
       redirect '/login'
@@ -11,10 +12,28 @@ class TransactionController < ApplicationController
 
   get '/transactions/shipping/new' do
     if logged_in?
-
       erb :"transactions/shipping/new"
     else
       redirect "/login"
+    end
+  end
+
+  post '/transactions' do
+    if !Transaction.where('reference = ? AND client_id = ?', params[:transaction_reference], params[:client_id])
+      flash.now[:notice] = "That client already has a transaction with that reference"
+      erb :"transactions/shipping/new"
+    else
+      transaction = Transaction.new
+      transaction.reference = params[:transaction_reference]
+      transaction.client_id = params[:client_id]
+      transaction.transaction_type = "shipping"
+      transaction.transaction_date = params[:transaction_date]
+      transaction.status = "open"
+      if transaction.save
+        redirect  '/transactions/shipping'
+      else
+        redirect '/transactions/shipping/new'
+      end
     end
   end
 end
